0c1f120d5b871360f4b29885321eddc3e71d9f76
Mohammed-Abol-Fotouh <moabolfotouh@gmail.com>
2025-01-28 22:29:04 +0200

    modify logic to show results in the UI

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// get computer choice
 function getComputerChoice() {
 	let choices = ["rock", "paper", "scissors"];
 	return choices.at(Math.floor(Math.random() * choices.length));
 }
 
 const buttons = document.querySelector(".selections");
-
 let playerSelection = null;
 let computerSelection = null;
 let computerScore = 0;
 let playerScore = 0;
+
+const round = document.querySelector(".round");
+const current = document.querySelector(".current");
+const winner = document.querySelector(".winner");
 
 buttons.addEventListener("click", (e) => {
 	switch (e.target.id) {
@@ -25,26 +27,22 @@ buttons.addEventListener("click", (e) => {
 	}
 	computerSelection = getComputerChoice();
 
-	// Play a round and log the result
 	const result = playRound(playerSelection, computerSelection);
-	console.log(result);
+	round.textContent = result;
 
-	// Log the current scores
-	console.log(
-		`Current Scores - Player: ${playerScore} Computer: ${computerScore}`,
-	);
+	current.textContent = `Current Scores - Player: ${playerScore} Computer: ${computerScore}`;
 
 	if (playerScore === 5 || computerScore === 5) {
-		console.log(
-			playerScore === 5 ? "You win the game!" : "Computer wins the game!",
-		);
+		winner.textContent =
+			playerScore === 5 ? "You win the game!" : "Computer wins the game!";
 
 		playerScore = 0;
 		computerScore = 0;
+		round.textContent = "";
+		current.textContent = "";
 	}
 });
 
-// play one round and save the score
 function playRound(playerSelection, computerSelection) {
 	if (playerSelection === computerSelection) {
 		return "It's a tie! Replay this round.";
